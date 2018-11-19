01aa0460145d798526e01d2f09862387da462de9
Bronson Avila <bronson.avila@gmail.com>
2018-11-19 00:48:15 -1000

    Fix position of inset box-shadow when card is open

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,5 @@
 const cardExterior = $('.card-exterior');
+const cardExteriorContent = $('.card-exterior-content');
 const cardExteriorContentFront = $('.card-exterior-content-front');
 const cardExteriorContentBack = $('.card-exterior-content-back');
 
@@ -6,12 +7,18 @@ function toggleCard() {
   if (!cardExterior.hasClass('open')) {
     cardExterior.addClass('open');
     window.setTimeout(() => {
+      cardExteriorContent.css({
+        'box-shadow': 'inset -1px 1px 2px hsla(270, 10%, 50%, 1)'
+      });
       cardExteriorContentFront.addClass('hide');
       cardExteriorContentBack.removeClass('hide');
     }, 165);
   } else {
     cardExterior.removeClass('open');
     window.setTimeout(() => {
+      cardExteriorContent.css({
+        'box-shadow': 'inset 1px 1px 2px hsla(270, 10%, 50%, 1)'
+      });
       cardExteriorContentFront.removeClass('hide');
       cardExteriorContentBack.addClass('hide');
     }, 225);
