3a577654522a108457addec4ed5a45ea66d86d48
Bronson Avila <bronson.avila@gmail.com>
2018-11-23 10:16:52 -1000

    Refactor (re: CSS class names and JS)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,42 +3,42 @@ const cardExteriorContent = $('.card-exterior-content');
 const cardExteriorContentFront = $('.card-exterior-content-front');
 const cardExteriorContentBack = $('.card-exterior-content-back');
 
-function toggleCard() {
-  // Open/close card on click/tap:
-  if (!cardExterior.hasClass('open')) {
-    cardExterior.addClass('open');
+function toggleCardOpenClose() {
+  // Open/close card on click/tap.
+  if (!cardExterior.hasClass('card-exterior__open')) {
+    cardExterior.addClass('card-exterior__open');
     window.setTimeout(() => {
-      cardExteriorContent.css({
-        'box-shadow': 'inset -1px 1px 2px hsla(270, 10%, 50%, 1)'
-      });
-      cardExteriorContentFront.addClass('hide');
-      cardExteriorContentBack.removeClass('hide');
+      cardExteriorContent.addClass('card_exterior-content__open');
+      cardExteriorContentFront.addClass('card_exterior-content__hidden');
+      cardExteriorContentBack.removeClass('card_exterior-content__hidden');
     }, 165);
   } else {
-    cardExterior.removeClass('open');
+    cardExterior.removeClass('card-exterior__open');
     window.setTimeout(() => {
-      cardExteriorContent.css({
-        'box-shadow': 'inset 1px 1px 2px hsla(270, 10%, 50%, 1)'
-      });
-      cardExteriorContentFront.removeClass('hide');
-      cardExteriorContentBack.addClass('hide');
+      cardExteriorContent.removeClass('card_exterior-content__open');
+      cardExteriorContentFront.removeClass('card_exterior-content__hidden');
+      cardExteriorContentBack.addClass('card_exterior-content__hidden');
     }, 225);
   }
 
-  // Remove click/tap listeners for 500ms (prevents click spamming):
+  preventClickSpam();
+}
+
+// Remove click/tap listeners for 500ms (prevents click spamming).
+function preventClickSpam() {
   $('.card').off('click');
   $('.card').off('tap');
   window.setTimeout(() => {
-    $('.card').click(toggleCard);
-    $('.card').on('tap', toggleCard);
-  }, 500);
+    $('.card').click(toggleCardOpenClose);
+    $('.card').on('tap', toggleCardOpenClose);
+  }, 500); // Value corresponds to .card-exterior "transition" attribute.
 }
 
-// Set initial event listeners:
-$('.card').click(toggleCard);
-$('.card').on('tap', toggleCard);
+// Set initial event listeners.
+$('.card').click(toggleCardOpenClose);
+$('.card').on('tap', toggleCardOpenClose);
 
-// Apply custom configuration settings to jQuery-Sakura:
+// Apply custom configuration settings to jQuery-Sakura.
 $(window).on('load', () => {
   $('body').sakura('start', {
     fallSpeed: 1.5,
